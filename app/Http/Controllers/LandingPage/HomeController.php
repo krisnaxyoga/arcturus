7cfda1ba0582c3db238812d26c38244bcc13a22f
muliadev3 <softhouse3@themulia.com>
2023-08-24 11:09:46 +0800

    .slider

--- app/Http/Controllers/LandingPage/HomeController.php
+++ app/Http/Controllers/LandingPage/HomeController.php
@@ -32,11 +32,13 @@ class HomeController extends Controller
 
                 $slider = Slider::where('user_id',$iduser)->get();
             }else{
-                $slider = Slider::where('user_id',1)->get();
+                // $slider = Slider::where('user_id',1)->get();
+                $slider = Slider::all();
             }
 
         }else{
-            $slider = Slider::where('user_id',1)->get();
+            // $slider = Slider::where('user_id',1)->get();
+            $slider = Slider::all();
         }
 
         $hotel = Vendor::where('type_vendor','hotel')->count();
