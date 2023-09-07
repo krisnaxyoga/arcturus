2a93f31ccc0313dc1af35a2bf8c695039ab7f2e7
muliadev3 <softhouse3@themulia.com>
2023-09-07 15:46:42 +0800

    up

--- resources/js/Pages/Vendor/BookingHistory/Detail.jsx
+++ resources/js/Pages/Vendor/BookingHistory/Detail.jsx
@@ -164,7 +164,7 @@ export default function Detail({ session, data,vendor, agent,roombooking,contrac
                                                                                     <td>{item.total_room}</td>
                                                                                     <td>{item.room.ratedesc} {roombooking.length > 1 ? (<span className='badge badge-dark'>{item.contractrate.codedesc}</span>):(<></>)}</td>
                                                                                     <td>{data.total_guests}</td>
-                                                                                    <td>{formatRupiah(item.pricenomarkup)}</td>
+                                                                                    <td>{formatRupiah(item.contractprice.recom_price)}</td>
                                                                                     <td>{data.night}</td>
                                                                                     <td>{formatRupiah(data.pricenomarkup)}</td>
                                                                                 </tr>
