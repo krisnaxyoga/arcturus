4f9f45262a6e53db650885a4c9b6ceaa1c94e4ef
krisnaxyoga <yogakris.yk@gmail.com>
2025-01-23 17:09:28 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,26 @@ class DatabaseSeeder extends Seeder
         Role::create([
             'role_name' => 'agent',
         ]);
-        
+
         User::factory(5)->create();
+
+
+        \App\Models\Setting::create([
+            'company_name' => 'Hotel Booking System Inc.',
+            'business_permit_number' => '123456789',
+            'description' => 'Hotel Booking System adalah sebuah aplikasi reservasi hotel yang dibangun menggunakan Laravel dan VueJS.',
+            'address_line1' => 'Jl. Jend. Sudirman No.1',
+            'address_line2' => 'Jakarta Pusat',
+            'country' => 'Indonesia',
+            'state' => 'DKI Jakarta',
+            'city' => 'Jakarta',
+            'telephone' => '081234567890',
+            'fax' => '081234567891',
+            'email' => 'admin@hotelbookingsystem.com',
+            'zipcode' => '12345',
+            'logo_image' => 'logo.png',
+            'url_logo_image' => 'https://hotelbookingsystem.com/logo.png',
+            'url_website' => 'https://hotelbookingsystem.com',
+        ]);
     }
 }
